356e52015effc5648a5c19c5f68a93ef362c4d46
luisanabenitez <81334728+luisanabenitez@users.noreply.github.com>
2022-07-09 13:55:20 -0300

    Add files via upload

---
+++ contacto/img/script.js
@@ -0,0 +1,16 @@
+// Validador de formulario
+
+
+$(document).ready(function(){
+
+  $("#cform").submit(function(){
+    $(this).find(".required").each(function(){
+      $flag = true;
+     if($(this).val()==""){
+       $flag = false;
+       $(this).addClass("empty").siblings(".red").children("small").html(" El campo es requerido");
+     }
+    });
+    return $flag; 
+  })
+})
